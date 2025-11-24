15e6c411a4d0bffaefe82544920186ebac5da8ba
superintendent2521 <67196752+superintendent2521@users.noreply.github.com>
2025-11-24 13:00:36 -0500

    increase upsert

--- content.js
+++ content.js
@@ -5,7 +5,7 @@ const CACHE_EXPIRY_DAYS = 30; // Cache for 30 days
 const SERVER_BASE_URL = 'https://twitter.superintendent.me'; // open source server, just has more users so it would be more accurate since more people add users into it.
 const SERVER_TIMEOUT_MS = 5000;
 const SERVER_LOOKUP_TTL_MS = 0; 
-const SERVER_UPSERT_TTL_MS = 30 * 1000; // Throttle writes to DB per user
+const SERVER_UPSERT_TTL_MS = 3 * 60 * 1000; // Throttle writes to DB per user
 
 // Hidden countries preferences
 const HIDDEN_COUNTRIES_KEY = 'hidden_countries';
